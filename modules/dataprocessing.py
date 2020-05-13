035b2051faedef02c01f3a74561ff83022e51fc9
michaelofsbu <jiachen.yao@stonybrook.edu>
2020-05-13 14:52:14 -0400

    merged industry type and filtered zipcode in NYC

--- modules/dataprocessing.py
+++ modules/dataprocessing.py
@@ -1,4 +1,5 @@
 import pandas as pd 
+import json
 
 # Sample data randomly
 def randomSample(df, frac=0.01):
@@ -12,13 +13,37 @@ def stratifiedSample(df, frac = 0.01):
 # Load data from url and process it
 def read(url):
     df = pd.read_csv(url)
+    # Drop unused collums
     df = df.drop(columns = ['Business Name 2', 'Address Building', 'Address Street Name', 
-                            'Secondary Address Street Name', 'Contact Phone Number', 'Borough Code',
+                            'Secondary Address Street Name', 'Contact Phone Number', 'Address Borough', 'Borough Code',
                             'Community Board', 'Council District', 'BIN', 'BBL', 'NTA', 'Census Tract',
                             'Detail', 'Longitude', 'Latitude', 'Location'])
+    # Industry type merge dictionary
+    industry_type = {'Amusement': ['Amusement Arcade', 'Amusement Device Permanent', 'Amusement Device Portable', 'Amusement Device Temporary'],
+                     'Garage and Parking Lot': ['Garage and Parking Lot', 'Garage', 'Parking Lot'],
+                     'Laundry': ['Laundry', 'Laundries', 'Laundry Jobber'],
+                     'Locksmith': ['Locksmith', 'Locksmith Apprentice'],
+                     'Secondhand Dealer': ['Secondhand Dealer - Auto', 'Secondhand Dealer - Firearms', 'Secondhand Dealer - General'],
+                     'Tow Truck': ['Tow Truck Company', 'Tow Truck Exemption']}
+    # Filter by zipcode in NYC
+    f = open('nycbyzipcode.json')
+    zip = json.load(f)
+    zipcode = []
+    for fea in zip['features']:
+        zipcode.append(fea['properties']['postalCode'])
+    f.close()
+    df = df[df['Address ZIP'].isin(set(zipcode))]
+
+    # Merge industry type
+    replace = {}
+    for key, value in industry_type.items():
+        for v in value:
+            replace[v] = key
+    df['Industry'] = df['Industry'].replace(replace)
+
+    # Sample
     df = stratifiedSample(df)
     df = df.dropna(axis='index', how='any')
-    df = df[df['Address State'] == 'NY']
     return df.reset_index(drop=True)
 
 if __name__ == '__main__':
