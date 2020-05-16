2719664f48f5e69f6bf2b87d2fc8b3ecad72460d
yma73 <yma73@binghamton.edu>
2020-05-16 13:45:15 -0400

    bar chart and map draft done

--- app.py
+++ app.py
@@ -1,31 +1,29 @@
-from flask import Flask, render_template, jsonify
+import json
+from flask import Flask, render_template, request, redirect, Response, jsonify
 from modules.dataprocessing import read
 import pandas as pd
+import modules.barChartandMapfunctions as bfuncs
 
 app = Flask(__name__)
 
-url = 'https://raw.githubusercontent.com/michaelofsbu/cse564_finaldata/master/Legally_Operating_Businesses.csv'
+url = 'processed_data.csv'
 global processed_data
-processed_data = read(url)
+processed_data = pd.read_csv(url)
 
 @app.route("/")
 def index():
-    
+
     return render_template("index.html")
 
-@app.route("/get_map_data/<argument>")
-def get_map_data(argument):
-    data = []
+@app.route("/get_map_data", methods = ["POST", "GET"])
+def get_map_data():
+    print(request.form["argument"])
+    data = json.dumps(bfuncs.get_map_info(processed_data, request.form["argument"]).to_dict(orient='records'))
     return jsonify(data)
 
-@app.route("/get_MDS_data/<argument>")
-def get_mds_data(argument):
-    data = []
-    return jsonify(data)
-
-@app.route("/get_barchart_data/<argument>")
-def get_barchart_data(argument):
-    data = []
+@app.route("/get_barchart_data", methods = ["POST", "GET"])
+def get_barchart():
+    data = json.dumps(bfuncs.count_frequency(processed_data).to_dict(orient='records'))
     return jsonify(data)
 
 @app.route("/get_stackchart_data/<Licensetype>")
